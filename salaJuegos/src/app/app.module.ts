9b2fc149f6bff4bf93549193dfb628d2f869be89
murraydemian <murraydemian@gmail.com>
2020-09-24 00:47:47 -0300

    agrego sidebar en ruta juegos

--- salaJuegos/src/app/app.module.ts
+++ salaJuegos/src/app/app.module.ts
@@ -9,11 +9,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { HomeComponent } from './component/home/home.component';
 import { AcercaDeComponent } from './component/acerca-de/acerca-de.component';
 import { JuegosComponent } from './component/juegos/juegos.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+
 
 
 @NgModule({
@@ -22,7 +31,8 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
     HomeComponent,
     AcercaDeComponent,
     JuegosComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    MainNavComponent
     
   ],
   imports: [
@@ -32,7 +42,12 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonToggleModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
